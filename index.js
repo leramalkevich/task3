4972d37a8b664803b259dab0628a73afb7f311fe
leramalkevich <leramalkevich@gmail.com>
2025-09-28 17:50:47 +0300

    changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 3000;
 const fetch = require('node-fetch');
 const email = "leramalkevich@gmail.com";
 const emailPath = emailTransformation(email).toString().trim();
+let currentUrl = '';
 
 function isNatural(n) {
     let num = Number(n);
@@ -19,31 +20,40 @@ function emailTransformation(email) {
     return email.replace(/[^A-Za-z0-9]/g, '_');
 }
 
-app.get('/', async (req, res) => {
-    res.redirect(`/${emailPath}`);
-    const currentUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+document.getElementById('button').addEventListener('click', (e) => {
+   let x = document.getElementById('x-number').value.toString();
+   let y = document.getElementById('y-number').value.toString();
     const urlObj = new URL(currentUrl);
-    urlObj.searchParams.set('x', '{}');
-    urlObj.searchParams.set('y', '{}');
-    const x = req.query.x;
-    const y = req.query.y;
-    if (!isNatural(x) || !isNatural(y)) {
-        res.send('NaN');
-        return;
-    }
-    let result = lcmCalculation(Number(x), Number(y));
-    const updatedUrl = urlObj.toString();
+    urlObj.searchParams.set('x', x);
+    urlObj.searchParams.set('y', y);
+});
+app.get('/', (req, res) => {
+    res.redirect(`/${emailPath}`);
+    currentUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
 
-    try {
-        // Выполняем GET-запрос на обновлённый URL
-        const response = await fetch(updatedUrl);
-        const data = await response.text(); // или response.json(), зависит от ответа
-        res.set('Content-Type', 'text/plain');
-        res.send(String(data));
+    // const currentUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    // const urlObj = new URL(currentUrl);
+    // urlObj.searchParams.set('x', '{}');
+    // urlObj.searchParams.set('y', '{}');
+    // const x = req.query.x;
+    // const y = req.query.y;
+    // if (!isNatural(x) || !isNatural(y)) {
+    //     res.send('NaN');
+    //     return;
+    // }
+    // let result = lcmCalculation(Number(x), Number(y));
+    // const updatedUrl = urlObj.toString();
+    //
+    // try {
+    //     Выполняем GET-запрос на обновлённый URL
+    //     const response = await fetch(updatedUrl);
+    //     const data = await response.text(); // или response.json(), зависит от ответа
+    //     res.set('Content-Type', 'text/plain');
+    //     res.send(String(data));
         // res.send(`Ответ с сервера по обновлённому URL:\n${data}`);
-    } catch (error) {
-        res.status(500).send(`Ошибка при запросе: ${error.message}`);
-    }
+    // } catch (error) {
+    //     res.status(500).send(`Ошибка при запросе: ${error.message}`);
+    // }
 });
 
 app.get(`/${emailPath}`, (req, res) => {
