9832b1f73a8a0233258e203accd3c6568cf0f154
leramalkevich <leramalkevich@gmail.com>
2025-09-30 12:39:10 +0300

    adds

--- index.js
+++ index.js
@@ -52,7 +52,8 @@ app.get('/', (req, res) => {
     res.redirect(url);
 });
 
-app.get(`/:emailPath{.x=${encodeURIComponent(x)}&y=${encodeURIComponent(y)}}`, (req, res) => {
+app.get(`/${emailPath}`, (req, res) => {
+// app.get(`/:emailPath{.x=${encodeURIComponent(x)}&y=${encodeURIComponent(y)}}`, (req, res) => {
     try {
         if (!req.query.x || !req.query.y) {
             res.type('text/plain').send('');
